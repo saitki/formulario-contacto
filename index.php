db5ef861883de169a75767ae947b733f67cda0ce
saitki <aleiretaxiu@gmail.com>
2025-02-06 15:56:58 -0500

    #Alix000004 new design bootstrap

--- index.php
+++ index.php
@@ -22,6 +22,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" sizes="32x32" href="./assets/images/favicon-32x32.png">
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+
     <title>Formulario de contacto</title>
 
     <style>
@@ -33,34 +35,70 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 <body>
 
-    <!-- Mensajes de éxito o error -->
-    <?php if (isset($errorMessage)) { echo "<p class='error'>$errorMessage</p>"; } ?>
+
+
+    <div class="card container w-50 p-5" style="width: 18rem; margin-top: 40px; margin-bottom: 40px;">
+     <form action="<?= $_SERVER['PHP_SELF'] ?>" class="formulario" enctype="multipart/form-data" method="post">
+        
+     <label for="name" class="form-label" style="font-size: 30px; margin-top:-40px;">Contactanos</label>
+     <div class="">
+        <div class="row ">
+        <div class="mb-3 col align-self-start">
+            <label for="name" class="form-label">Nombre</label>
+            <input type="text" class="form-control" name="name" id="name" aria-describedby="nameHelp" required>
+            <div id="nameHelp" class="form-text"></div>
+        </div>
+        <div class="mb-3 col">
+            <label for="lastname" class="form-label">Apellido</label>
+            <input type="text" class="form-control"  name="lastname" id="lastname" aria-describedby="lastnameHelp" required>
+            <div id="lastnameHelp" class="form-text"></div>
+        </div>
+        </div>
+    </div>   
+     
+
+
+        <div class="mb-3">
+            <label for="email" class="form-label">Correo electrónico</label>
+            <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" required>
+            <div id="emailHelp" class="form-text"></div>
+        </div>
+
+        <div class=" mb-3 ">
+            <div class="row ">
+            <label  class="form-label">Selecciona una opción</label><br>
+
+                <div class="form-check col" style="margin-left: 10px;">
+                <input class="form-check-input" type="radio" id="flexRadioDefault1" name="option" value="1" checked>
+                <label class="form-check-label" for="flexRadioDefault1"> Opción 1 </label>
+
+                </div>
+                <div class="form-check col" style="margin-left: 10px;">
+                <input class="form-check-input" type="radio" name="option" id="flexRadioDefault2"  value="2" >
+                <label class="form-check-label" for="flexRadioDefault2">Opción 2 </label>
+                </div>
+            </div>
+    </div>   
+
+
+       
+        <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Mensaje</label>
+             <textarea class="form-control" name="message" id="exampleFormControlTextarea1" rows="3" required></textarea>
+        </div>
+        
+        <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        <!-- Mensajes de éxito o error -->
+        <?php if (isset($errorMessage)) { echo "<p class='error'>$errorMessage</p>"; } ?>
     <?php if (isset($successMessage)) { echo "<p class='success'>$successMessage</p>"; } ?>
 
-    <!-- Formulario -->
-    <form action="<?= $_SERVER['PHP_SELF'] ?>" class="formulario" enctype="multipart/form-data" method="post">
-        <label for="name">Nombre</label>
-        <input type="text" name="name" value="" required>
 
-        <label for="lastname">Apellido</label>
-        <input type="text" name="lastname" value="" required>
-
-        <label>Selecciona una opción:</label><br>
-        <input type="radio" name="option" value="1" id="option1" required>
-        <label for="option1">Opción 1</label>
-
-        <input type="radio" name="option" value="2" id="option2">
-        <label for="option2">Opción 2</label>
-
-        <label for="email">Correo electrónico</label>
-        <input type="text" name="email" value="" required>
-
-        <label for="message">Mensaje</label>
-        <textarea name="message" required></textarea>
-
-        <input type="submit" value="Enviar">
-    </form>
-
+    </div>
     <div class="autor">
         Formulario de contacto @2025. Desarrollado por <a href="#">Alejandro Lenier Ireta Xiu</a>.
     </div>
