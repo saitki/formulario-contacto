117490ffe5da93d1116e7529634fe974406db683
saitki <aleiretaxiu@gmail.com>
2025-02-06 00:09:14 -0500

    #000001Alix Add Db

---
+++ DB/db.php
@@ -0,0 +1,14 @@
+<?php
+
+function conexion($bd_config){
+	try {
+	$conexion = new PDO('mysql:host=localhost;dbname='.$bd_config['basedatos'], $bd_config['usuario'], $bd_config['pass']);
+	$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		return $conexion;
+
+	} catch (PDOException $e) {
+		return false;
+	}
+}
+?>
